f9cf7a12dd3e293155b8c05ad270cce49902a984
jeeravan <pranaypandit12@gmail.com>
2025-08-11 05:59:26 +0530

    deploy

--- nova_manager/core/config.py
+++ nova_manager/core/config.py
@@ -2,7 +2,7 @@ import os
 from os import getenv
 from dotenv import load_dotenv
 
-load_dotenv(os.path.join(os.getcwd(), ".env"), override=True)
+load_dotenv(os.path.join(os.getcwd(), ".env"), override=False)  # don’t overwrite existing OS env vars
 
 
 DATABASE_URL = getenv("DATABASE_URL") or ""
